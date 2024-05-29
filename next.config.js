3027aee1ad8a617597aa423a58dee120e05c3bf6
excaangga <excaangga@gmail.com>
2024-05-29 18:40:07 +0700

    mlajo early web commit

--- next.config.js
+++ next.config.js
@@ -3,13 +3,10 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-module.exports = withBundleAnalyzer({
-  poweredByHeader: false,
-  trailingSlash: true,
-  basePath: '',
-  distDir: 'build',
-  // The starter code load resources from `public` folder with `router.basePath` in React components.
-  // So, the source code is "basePath-ready".
-  // You can remove `basePath` if you don't need it.
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
   reactStrictMode: true,
-});
+};
+
+module.exports = nextConfig;
